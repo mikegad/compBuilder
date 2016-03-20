96711990aa19548dc3a01a703d25c940ee12588b
srodens <sr.odens@gmail.com>
2016-03-20 09:53:45 -0400

    add js and description to computers

---
+++ js/compBuilder.js
@@ -0,0 +1,34 @@
+			$(document).ready(function() {
+				$(".modelD5B1").hover(function() {
+					$('#modelD5B1').show();
+				}, function() {
+					$('#modelD5B1').hide();
+				});
+				$(".modelD0B2").hover(function() {
+					$('#modelD0B2').show();
+				}, function() {
+					$('#modelD0B2').hide();
+				});
+				$(".modelDDB3").hover(function() {
+					$('#modelDDB3').show();
+				}, function() {
+					$('#modelDDB3').hide();
+				});
+				$(".modelL5B1").hover(function() {
+					$('#modelL5B1').show();
+				}, function() {
+					$('#modelL5B1').hide();
+				});
+				$(".modelL0B2").hover(function() {
+					$('#modelL0B2').show();
+				}, function() {
+					$('#modelL0B2').hide();
+				});
+				$(".modelLB32").hover(function() {
+					$('#modelLB32').show();
+				}, function() {
+					$('#modelLB32').hide();
+				});
+			});
+
+
